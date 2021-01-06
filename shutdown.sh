1cc8c57e038c842e4651450efb74cd072adff653
barblin <johannes.preisinger@gmail.com>
2021-01-06 19:02:42 +0100

    [JPA-06] verify that data is persisted and exercise camunda rest api with postman collection

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A simple script
 
-docker-compose down
+docker-compose --env-file ./config/.env.dev down
 rm -r ./com.alanda.camunda.services/target
 rm -r ./com.alanda.process.services/target
